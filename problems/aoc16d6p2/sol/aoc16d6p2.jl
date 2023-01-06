316418bce94a4b3f5e76503305fb58eab77b8a76
czylabsonasa <czylabson.asa@gmail.com>
2023-01-06 13:16:44 +0100

    16/6

---
+++ problems/aoc16d6p2/sol/aoc16d6p2.jl
@@ -0,0 +1,30 @@
+##############################################################
+# advent of code 16 day 6 part 2
+
+using DataStructures
+function solve(fin,fout)
+
+  readit(input)=mapreduce(
+    permutedims,
+    vcat,
+    split(read(input,String),'\n',keepempty=false).|>collect
+  ) 
+
+  A=readit(fin)
+
+  # solve(v)=argmax(x->x[2],counter(v))[1]
+  solve(v)=argmin(x->x[2],counter(v))[1]
+  
+
+  println(
+    fout,
+    join(solve.(eachcol(A)))
+  )
+
+end # of solve(fin,fout)
+
+
+
+if abspath(PROGRAM_FILE)==@__FILE__
+  solve(stdin,stdout)
+end
